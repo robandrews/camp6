81487405b2b24970f441da6d22fbf05ae77c6d90
Rob Andrews <rob.andrews@alumni.stanford.edu>
2014-03-28 10:09:26 -0700

    best version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140326164704) do
+ActiveRecord::Schema.define(version: 20140328165311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,12 +71,12 @@ ActiveRecord::Schema.define(version: 20140326164704) do
   add_index "todos", ["todo_list_id"], name: "index_todos_on_todo_list_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",                                                           null: false
+    t.string   "encrypted_password",     default: "",                                                           null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,                                                            null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -85,7 +85,7 @@ ActiveRecord::Schema.define(version: 20140326164704) do
     t.string   "lname"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "picture_url"
+    t.string   "picture_url",            default: "https://s3-us-west-1.amazonaws.com/camp6/img/avatar.40.gif"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
