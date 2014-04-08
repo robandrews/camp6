80504a4a7d84f082b6f6ef77307f78ebfe716a5c
Rob Andrews <rob.andrews@alumni.stanford.edu>
2014-04-07 18:51:22 -0700

    continuing to update test prior to refactor

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,7 +2,12 @@ require "spec_helper"
 
 
 describe Project do
-  subject(:project){Project.new}
+  subject(:project){Project.create(owner_id: 1, title: "Project 1", subtitle:"a cool project")}
+  subject(:user){User.create(fname: "Rob", lname: "Andrews", email: "rob@gmail.com", password: "password",
+    picture_url: "https://pbs.twimg.com/profile_images/378800000785341088/f402f34cbbb39acdf06cdc4988196e55.jpeg")}  
+  subject(:user2){User.create(fname: "Ryan", lname: "O'Donnell", email: "ryan@gmail.com", password: "password",
+    picture_url: "http://spinoff.comicbookresources.com/wp-content/uploads/2013/12/terminator.jpg")}
+    
   it "has project memberships" do
     expect(project).to respond_to(:project_memberships)
   end
@@ -15,10 +20,14 @@ describe Project do
     expect(project).to respond_to(:notes)
   end
   
-  it "can add users" do
-    user = User.create!(fname: "Rob", lname: "Andrews", email:"rob@gmail.com", password:"password")
-    project.users.build(user)
-    expect(project.users.first).to eq(user)
+  it "belongs to users" do
+    user.projects.create(owner_id: 1, title: "Project 1", subtitle:"a cool project")
+    expect(user.projects).to_not be(nil)
+  end
+  
+  it "can add other users" do
+    user2.projects.concat(project)
+    expect(user2.projects).to include(project)
   end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe User do
-  
+  subject(:user){}
   it "requires an email" do
     user = User.new(fname: "Rob", lname: "Andrews", password:"password")
     expect(user).to_not be_valid
@@ -15,5 +15,11 @@ describe User do
   it "has a name" do
      user = User.create!(fname: "Rob", lname: "Andrews", email: "rob@gmail.com", password:"password")
      expect(user.name).to eq("Rob Andrews")
+  end
+  
+  it "can have notes" do
+    user = User.create(fname: "Rob", lname: "Andrews", email:"rob@gmail.com", password:"password")
+    user.notes.create(body: 'test', project_id:1)
+    expect(user.notes).to_not be(nil)
   end
 end
